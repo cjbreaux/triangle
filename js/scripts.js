be380026f046a066e0b654f63edc3373a49da845
Chris Breaux and Nate Wessels <nate@>
2019-01-17 16:31:12 -0800

    add conditional statement to determine what type of triangle, and show alert to user

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,15 @@ $(document).ready(function() {
     var thirdSide = $("#thirdSide").val()
     thirdSide = parseFloat(thirdSide);
 
-    alert(thirdSide);
+    if (firstSide + secondSide <= thirdSide || secondSide + thirdSide <= firstSide || firstSide + thirdSide <= secondSide) {
+      alert("Not A Triangle");
+    } else if (firstSide === secondSide && secondSide === thirdSide) {
+      alert("This is a Equilateral Triangle");
+    } else if(firstSide === secondSide || secondSide === thirdSide || thirdSide === firstSide) {
+      alert("This is an Isosceles Triangle");
+    } else if(firstSide !== secondSide && secondSide !== thirdSide && thirdSide !== firstSide) {
+      alert("This is a Scalene Triangle");
 
-
-
-
-
+    }
   });
 });
