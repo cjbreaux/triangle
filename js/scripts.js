11f8ffa004526584cce88acc86f1a6e7946b4caf
Chris Breaux and Nate Wessels <chris@>
2019-01-17 16:38:23 -0800

    add conditional statement that checks if user did not input number in any input

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
     var thirdSide = $("#thirdSide").val()
     thirdSide = parseFloat(thirdSide);
 
-    if (firstSide + secondSide <= thirdSide || secondSide + thirdSide <= firstSide || firstSide + thirdSide <= secondSide) {
+    if(firstSide, secondSide, thirdSide) {
+      if (firstSide + secondSide <= thirdSide || secondSide + thirdSide <= firstSide || firstSide + thirdSide <= secondSide) {
       alert("Not A Triangle");
     } else if (firstSide === secondSide && secondSide === thirdSide) {
       alert("This is a Equilateral Triangle");
@@ -15,7 +16,9 @@ $(document).ready(function() {
       alert("This is an Isosceles Triangle");
     } else if(firstSide !== secondSide && secondSide !== thirdSide && thirdSide !== firstSide) {
       alert("This is a Scalene Triangle");
-
     }
+    }else {
+      alert("No length given! Re-try");
+      }
   });
 });
